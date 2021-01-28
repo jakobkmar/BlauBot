9ac197a06f2827c3e2b2bd79e55c95dc751753c6
bluefireoly <bluefireolymp@gmail.com>
2021-01-28 18:17:55 +0100

    Create ConfigManager.kt

---
+++ src/main/kotlin/net/axay/blaubot/config/ConfigManager.kt
@@ -0,0 +1,20 @@
+package net.axay.blaubot.config
+
+import com.typesafe.config.ConfigFactory
+import io.github.config4k.extract
+import net.axay.blaubot.config.data.DiscordApplication
+import java.io.File
+
+object ConfigManager {
+
+    class ConfigFile(path: String) : File(path) {
+        init {
+            if (!parentFile.exists()) parentFile.mkdirs()
+            if (!exists()) createNewFile()
+        }
+    }
+
+    val discordApplication =
+        ConfigFactory.parseFile(ConfigFile("./config/discordApplication.conf")).extract<DiscordApplication>()
+
+}
