4e46129b2f4f13f3683d7dbf7ea7fcd666b608f1
royzer <royzerfisch@gmail.com>
2021-01-28 19:06:00 +0100

    add BingusCommand

---
+++ src/main/kotlin/net/axay/blaubot/commands/Bingus.kt
@@ -0,0 +1,21 @@
+package net.axay.blaubot.commands
+
+import dev.kord.common.annotation.KordPreview
+import dev.kord.core.behavior.followUp
+import dev.kord.core.entity.interaction.Interaction
+import net.axay.blaubot.commands.api.SlashCommand
+
+@KordPreview
+object Bingus: SlashCommand(
+    "bingus",
+    "get bingus"
+) {
+    override suspend fun handleCommand(interaction: Interaction) {
+        interaction.acknowledge(true).followUp {
+            embed {
+                title = "Bingus"
+                image = "https://media.tenor.co/videos/e37c4423a3db034d561e1a7c0fc2e4f9/mp4"
+            }
+        }
+    }
+}
