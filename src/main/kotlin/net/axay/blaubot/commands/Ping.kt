f53f45419028160df34efdce6ce8e1ce7360035c
bluefireoly <bluefireolymp@gmail.com>
2021-01-28 14:27:32 +0100

    Create Ping.kt

---
+++ src/main/kotlin/net/axay/blaubot/commands/Ping.kt
@@ -0,0 +1,26 @@
+package net.axay.blaubot.commands
+
+import com.gitlab.kordlib.kordx.emoji.Emojis
+import dev.kord.common.annotation.KordPreview
+import dev.kord.core.entity.interaction.Interaction
+import net.axay.blaubot.commands.api.SlashCommand
+import kotlin.random.Random
+
+@KordPreview
+object Ping : SlashCommand(
+    "ping",
+    "Play table tennis with the bot"
+) {
+
+    override suspend fun handleCommand(interaction: Interaction) {
+        interaction.acknowledge(true)
+        if (Random.nextInt(6) == 1) {
+            interaction.channel.createMessage("Peng!")
+            interaction.channel.createMessage("${Emojis.fullMoonWithFace}${Emojis.gun}")
+        } else {
+            interaction.channel.createMessage("${Emojis.pingPong}")
+            interaction.channel.createMessage("Pong! ${Emojis.grinning}")
+        }
+    }
+
+}
