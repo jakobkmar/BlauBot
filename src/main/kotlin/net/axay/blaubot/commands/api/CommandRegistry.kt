13a98b902d2cd3676227b60137949a7dcfba1169
Jakob K <bluefireolymp@gmail.com>
2021-05-14 02:14:31 +0200

    Update CommandRegistry.kt

--- src/main/kotlin/net/axay/blaubot/commands/api/CommandRegistry.kt
+++ src/main/kotlin/net/axay/blaubot/commands/api/CommandRegistry.kt
@@ -1,13 +1,14 @@
 package net.axay.blaubot.commands.api
 
 import dev.kord.common.annotation.KordPreview
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.behavior.createApplicationCommand
+import dev.kord.core.behavior.createApplicationCommands
 import dev.kord.core.event.interaction.InteractionCreateEvent
 import dev.kord.core.on
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 @KordPreview
@@ -15,22 +16,26 @@ object CommandRegistry {
     val registeredCommands = HashMap<String, SlashCommand>()
 
     suspend fun applyToBot(kord: Kord) = coroutineScope {
-        kord.guilds.map { guild ->
-            launch {
-                guild.commands.map {
-                    launch {
-                        it.delete()
-                    }
-                }.collect { it.join() }
+        for ((_, slashCommand) in registeredCommands) {
+            if (!slashCommand.test)
+                kord.createGlobalApplicationCommand(slashCommand.name, slashCommand.description, slashCommand.builder)
+        }
+        println("Set up global commands")
 
-                registeredCommands.map { (_, slashCommand) ->
-                    launch {
-                        guild.createApplicationCommand(slashCommand.name, slashCommand.description, slashCommand.builder)
-                    }
-                }.forEach { it.join() }
-                println("Finished editing guild ${guild.name}")
+        val testguild = kord.getGuild(Snowflake(738122976280707125))
+        if (testguild != null) {
+            for ((_, slashCommand) in registeredCommands) {
+                if (slashCommand.test)
+                    testguild.createApplicationCommand(slashCommand.name, slashCommand.description, slashCommand.builder)
             }
-        }.collect { it.join() }
+            launch {
+                testguild.commands.collect {
+                    if (!registeredCommands.containsKey(it.name))
+                        it.delete()
+                }
+            }
+            println("Set up test guild")
+        }
 
         kord.on<InteractionCreateEvent> {
             registeredCommands[interaction.command.rootName]?.execute(interaction, interaction.command)
