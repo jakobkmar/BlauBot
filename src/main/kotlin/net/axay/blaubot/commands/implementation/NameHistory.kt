e9409cf9f09889a5dc54a8a52840ef3b57976ec5
Oskar Schulz <53192281+vollkorncrafter@users.noreply.github.com>
2021-09-24 16:34:17 +0200

    Create NameHistory.kt
    codet by taito

---
+++ src/main/kotlin/net/axay/blaubot/commands/implementation/NameHistory.kt
@@ -0,0 +1,29 @@
+package net.axay.blaubot.commands.implementation
+
+import dev.kord.common.annotation.KordPreview
+import dev.kord.core.behavior.interaction.followUp
+import dev.kord.core.entity.interaction.CommandInteraction
+import dev.kord.core.entity.interaction.InteractionCommand
+import dev.kord.core.entity.interaction.string
+import dev.kord.rest.builder.interaction.embed
+import net.axay.blaubot.commands.api.SlashCommand
+
+@KordPreview
+object GithubProfile : SlashCommand(
+    "githubprofile",
+    "Shows you the Minecraft name history of a given user",
+    {
+        string("user", "The name of the user")
+    }
+) {
+    override suspend fun execute(interaction: CommandInteraction, command: InteractionCommand) {
+        interaction.acknowledgePublic().followUp {
+            embed {
+                val user = command.options["user"]?.string().orEmpty()
+                title = user
+                description = "This is the Minecraft name history of $user"
+                image = "https://files.vollkorn.me/TaitoNameHistory/?user=$user"
+            }
+        }
+    }
+}
